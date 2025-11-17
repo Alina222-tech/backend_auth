8019e412b5071589b0ac36fd48ee6902322ce4df
Alina <taurusalinasheikh@gmail.com>
2025-11-17 14:34:21 +0500

    add

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
   credentials: true,
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin:"http://localhost:5173",
 }));
 
 app.use(fileupload())

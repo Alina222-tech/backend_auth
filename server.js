8dc6f2ad87b5d9af8f58bd0f76fc07161cf0c2da
Alina <taurusalinasheikh@gmail.com>
2025-11-17 21:43:41 +0500

    add

--- server.js
+++ server.js
@@ -5,25 +5,27 @@ const cors = require("cors");
 const fileupload = require("express-fileupload");
 
 dotenv.config();
-
 const app = express();
 const routes = require("./routes/userrouter.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", // local frontend
+  credentials: true,
+}));
 
 app.use(fileupload());
-
 app.use("/api/user", routes);
 
-app.get("/",(req,res)=>{
-  res.send("server is running.")
-})
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("Database connected"))
-.catch(() => console.log("Database NOT connected"));
+app.get("/", (req, res) => res.send("Server is running"));
 
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Database connected"))
+.catch((err) => console.log("Database NOT connected:", err));
 
 module.exports = app;
